b9c239f81614ac3a7987cefb3c169aafd7e505a0
franky <2971684021@qq.com>
2026-02-05 10:07:41 +0800

    feat: add Result (Propagating)

--- src/chapter-9/02-result.rs
+++ src/chapter-9/02-result.rs
@@ -4,8 +4,9 @@
 //     Err(E),
 // }
 
-use core::error;
-use std::{fs::File, io::ErrorKind};
+use std::fs::File;
+use std::io::{self, Read};
+use std::io::ErrorKind;
 
 fn main() {
     // part 1
@@ -48,5 +49,43 @@ fn main() {
     // part4
     // unwrap() expect() panic的快捷方式
     // let get_file_result4 = File::open("Hello.txt").unwrap();
-    let get_file_result4_1 = File::open("Hello.txt").expect("panic!!");
+    // let get_file_result4_1 = File::open("Hello.txt").expect("panic!!");
+
+    let _ = read_from_file();
+}
+
+// 传播错误
+fn read_from_file() -> Result<String, io::Error> {
+    let get_file_result5 = File::open("Hello.txt");
+
+    let mut file_text = match get_file_result5 {
+        Ok(file) => file,
+        Err(e) => {
+            println!("file_text");
+            return Err(e);
+        }
+    };
+
+    let mut file_str = String::new();
+
+    match file_text.read_to_string(&mut file_str) {
+        Ok(_) => {
+            println!("{}", file_str);
+            return Ok(file_str);
+        }
+        Err(e) => {
+            println!("file_str");
+            return Err(e);
+        }
+    }
+}
+
+// ?运算符 传播错误的快捷方式
+// read_from_file2()与read_from_file()等效
+fn read_from_file2() -> Result<String, io::Error> {
+    let mut get_result_file6 = File::open("Hello.txt")?;
+    let mut file_str = String::new();
+    get_result_file6.read_to_string(&mut file_str)?;
+    // File::open("Hello.txt")?.read_to_string(&mut file_str)?;
+    Ok(file_str)
 }
